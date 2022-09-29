c154028655d402b4fe33f59de3d718db8b32244e
lesson18 <neofun777@gmail.com>
2022-09-29 20:39:54 +0400

    third commit

--- js19.js
+++ js19.js
@@ -1,34 +1,9 @@
 $(document).ready(function() {
-    var getSlide = $(".swiper-wrapper .nuts-active").data("nuts");
-
-    if (getSlide === undefined || getSlide === null){
-        var idSlide = 0;
-    } else {
-        var idSlide = getSlide;
-    }
-
-    console.log(getSlide);
-
-    var swiper = new Swiper('.swiper-container', {
-        hashNavigation: {
-            watchState: true,
-        },
-        slidesPerView: 1,
-        spaceBetween: 30,
+// Swiper: Slider
+    new Swiper('.swiper-container', {
         loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-
-
-
-        initialSlide: idSlide,
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev'
     });
-
-
 });
